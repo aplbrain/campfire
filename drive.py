eaf517847318522357597eceddd19af3708caefc
joycejm1 <justin.joyce@jhuapl.edu>
2022-05-11 12:20:51 -0400

    script now can run indefinitely, pulling from the queue

--- drive.py
+++ drive.py
@@ -5,7 +5,7 @@ from cloudvolume import CloudVolume
 import aws.sqs as sqs
 import sys
 
-def run_endpoints(root_id,radius=(100,100,10), resolution=(8,8,40), unet_bound_mult=2, return_opacity_seg=False,save_format='pickle',delete=False):
+def run_endpoints(root_id, save_format='pickle',delete=False,return_skel=False):
     # SPINE FINDING
 
     client = CAVEclient('minnie65_phase3_v1')
@@ -14,7 +14,7 @@ def run_endpoints(root_id,radius=(100,100,10), resolution=(8,8,40), unet_bound_m
         root_id_msg = sqs.get_job_from_queue(queue_url)
         root_id = int(root_id_msg.body)
     print("ROOT_ID", root_id)
-    end_points = spine_finding.find_endpoints(root_id, 
+    end_points = spine_finding.find_endpoints(root_id,
                                client=client,
                                refine='all',
                                root_point_resolution=[4, 4, 40],
@@ -33,6 +33,33 @@ def run_endpoints(root_id,radius=(100,100,10), resolution=(8,8,40), unet_bound_m
     elif save_format == 'pickle':
         import pickle
         pickle.dump(end_points, open(f"./data/{root_id}_endpoints.p", "wb"))
+
+def run_endpoints_sqs(root_id, end, save_format='pickle',delete=False,return_skel=False):
+    client = CAVEclient('minnie65_phase3_v1')
+    queue_url_rid = sqs.get_or_create_queue("Root_ids_endpoints")
+    queue_url_endpts = sqs.get_or_create_queue('Endpoints')
+
+    root_id_msg = sqs.get_job_from_queue(queue_url_rid)
+    root_id = int(root_id_msg.body)
+    n_root_id = 0
+    if end == -1:
+        end = 1e10
+    while n_root_id < end:
+        print("ROOT_ID", root_id)
+        end_points = spine_finding.find_endpoints(root_id,
+                                client=client,
+                                refine='all',
+                                root_point_resolution=[4, 4, 40],
+                                collapse_soma=True,
+                                n_parallel=8)
+
+        entries=sqs.construct_endpoint_entries(end_points, root_id)
+        while(len(entries) > 0):
+            entries_send = entries[:10]
+            entries = entries[10:]
+            sqs.send_batch(queue_url_endpts, entries_send)
+        if delete:
+            root_id_msg.delete()
 
 if __name__ == "__main__":
     mode = sys.argv[1]
