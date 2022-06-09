85e4b2c4dd7713f3f80f3e7e821f6c42b4740583
ThaumielSparrow <lzhang1337@gmail.com>
2022-06-09 16:53:03 -0400

    bugfixes in finding_orphans

--- finding_orphans.py
+++ finding_orphans.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 
 
 # Gets all the seg ids within a given subvolume and organizes by size of process
-def get_unqiue_seg_ids_em(x_min, x_max, y_min, y_max, z_min, z_max):
+def get_unique_seg_ids_em(x_min, x_max, y_min, y_max, z_min, z_max):
 
     # Get entire EM data - uncomment after testing
     # em = CloudVolume('s3://bossdb-open-data/iarpa_microns/minnie/minnie65/seg', use_https=True, mip=0, parallel=True, fill_missing=True, progress=True)
@@ -22,7 +22,12 @@ def get_unqiue_seg_ids_em(x_min, x_max, y_min, y_max, z_min, z_max):
     unique_seg_ids_sv = np.unique(seg_ids_sv)
 
     # Removing the first element (artifacts) of unique_seg_ids_sv
+    """
+    Drop all zeros in array instead of only first, handles edge cases
     unique_seg_ids_sv = np.delete(unique_seg_ids_sv, 0)
+    """
+
+    unique_seg_ids_sv = unique_seg_ids_sv[unique_seg_ids_sv != 0]
 
     # Organizing seg ids in subvolume by size
     seg_ids_by_size = {}
@@ -37,7 +42,7 @@ def get_unqiue_seg_ids_em(x_min, x_max, y_min, y_max, z_min, z_max):
 
 # Get the list of orphans within a given subvolume organized by largest orphan in subvolume first
 def get_orphans(x_min, x_max, y_min, y_max, z_min, z_max):
-    unique_seg_ids = get_unqiue_seg_ids_em(
+    unique_seg_ids = get_unique_seg_ids_em(
         x_min, x_max, y_min, y_max, z_min, z_max)
 
     # Getting all the orphans
@@ -54,15 +59,15 @@ def get_orphans(x_min, x_max, y_min, y_max, z_min, z_max):
 # Input: processes is a dictionary with key = seg_id, value = list of attributes
 # Returns: updated processes so that value also includes the type of the process
 def get_process_type(processes):
-    for process in tqdm(processes.items()):
+    for seg_id, attributes in processes.items():
         num_pre_synapses, num_post_synapses = data_loader.get_syn_counts(
-            str(processes[0]))
+            str(seg_id))
         if (num_pre_synapses > num_post_synapses):
-            process[1].append('axon')
+            attributes.append('axon')
         elif (num_post_synapses > num_pre_synapses):
-            process[1].append('dendrite')
+            attributes.append('dendrite')
         else:
-            process[1].append('unconfirmed')
+            attributes.append('unconfirmed')
     return processes
 
 
@@ -75,5 +80,6 @@ if __name__ == "__main__":
     z_max = 21315
     orphans = get_orphans(x_min, x_max, y_min, y_max, z_min, z_max)
     print("Number of orphans:", len(orphans))
+    print(type(orphans))
     get_process_type(orphans)
     print("Orphans", orphans)
