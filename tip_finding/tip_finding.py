11e5af5e65a5dfde80db39908e17d36d169d68cd
Rupa Chalavadi <RupaChalavadi@MacBook-Pro-4.local>
2022-06-29 11:12:20 -0400

    updated tip_finding.py to include Hunfrey smoothing, also edited test_tip_finding.ipnb

--- tip_finding/tip_finding.py
+++ tip_finding/tip_finding.py
@@ -4,45 +4,58 @@ from caveclient import CAVEclient
 import trimesh
 import numpy as np
 
-def tip_finder_decimation(root_id, nucleus_id=None, time=None, pt_position=None, sqs_queue_name = "None", save_df=False, save_nvq=False):
+
+def tip_finder_decimation(root_id, nucleus_id=None, time=None, pt_position=None, sqs_queue_name="None", save_df=False, save_nvq=False):
     datastack_name = "minnie65_phase3_v1"
     client = CAVEclient(datastack_name)
     vol = CloudVolume(
-            client.info.segmentation_source(),
-            use_https=True,
-            progress=False,
-            bounded=False,
-            fill_missing=True,
-            secrets={"token": client.auth.token}
-        )
+        client.info.segmentation_source(),
+        use_https=True,
+        progress=False,
+        bounded=False,
+        fill_missing=True,
+        secrets={"token": client.auth.token}
+    )
     mesh = vol.mesh.get(str(root_id))
     mesh = mesh[int(root_id)]
     n_faces = mesh.faces.shape[0]
     mesh_obj = trimesh.Trimesh(mesh.vertices, mesh.faces, mesh.normals)
-    decimated = trimesh.Trimesh.simplify_quadratic_decimation(mesh_obj, n_faces*.05)
+    # test with humphrey
+    # mesh_obj = trimesh.smoothing.filter_humphrey(mesh_obj, iterations=50)
 
-    edges_by_component=trimesh.graph.connected_component_labels(decimated.face_adjacency)
+    # test with laplacian
+    # mesh_obj = trimesh.smoothing.filter_laplacian(mesh_obj, iterations=50)
+    decimated = trimesh.Trimesh.simplify_quadratic_decimation(
+        mesh_obj, n_faces*.50)
+
+    edges_by_component = trimesh.graph.connected_component_labels(
+        decimated.face_adjacency)
     largest_component = np.bincount(edges_by_component).argmax()
-    largest_component_size = np.sum(edges_by_component==largest_component)
+    largest_component_size = np.sum(edges_by_component == largest_component)
 
-    cc = trimesh.graph.connected_components(decimated.face_adjacency, min_len=largest_component_size-1)
+    cc = trimesh.graph.connected_components(
+        decimated.face_adjacency, min_len=largest_component_size-1)
+
+    # cc = trimesh.graph.connected_components(
+    #     decimated.face_adjacency)
 
     mask = np.zeros(len(decimated.faces), dtype=bool)
 
     mask[np.concatenate(cc)] = True
 
     decimated.update_faces(mask)
-    skel = skeletonize.skeletonize_mesh(trimesh_io.Mesh(decimated.vertices, 
-                                                                decimated.faces))
+    skel = skeletonize.skeletonize_mesh(trimesh_io.Mesh(
+        decimated.vertices, decimated.faces), invalidation_d=5000)
 
     degree_dict = {}
     unique_ids = np.unique(skel.edges)
     for i in unique_ids:
         degree_dict[i] = np.sum(skel.edges == i)
-        
+
     degree = np.array(list(degree_dict.values()))
-    points = skel.vertices[np.argwhere(degree==1)].astype(int)
-    tl = [tuple([int(e[0][0])//4, int(e[0][1])//4, int(e[0][2])//40]) for e in points]
+    points = skel.vertices[np.argwhere(degree == 1)].astype(int)
+    tl = [tuple([int(e[0][0])//4, int(e[0][1])//4, int(e[0][2])//40])
+          for e in points]
 
     # import meshparty
     # import boto3
@@ -56,5 +69,5 @@ def tip_finder_decimation(root_id, nucleus_id=None, time=None, pt_position=None,
     # except Exception as e:
     #     print(e, "s3 upload failed")
     #     save_skel = 'h5'
-    
-    return tl, skel
+
+    return tl, skel, mesh_obj
